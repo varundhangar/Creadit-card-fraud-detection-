3d9253e171e750028fc0c81e12e517a574ffd928
varundhangar <varundhangar99775@gmail.com>
2025-09-23 13:51:18 +0000

    Update code and dependencies

--- app.py
+++ app.py
@@ -6,10 +6,11 @@ from sklearn.linear_model import LogisticRegression
 from sklearn.feature_extraction.text import TfidfVectorizer
 import joblib
 
-#from sklearn.neighbors import KNeighborsClassifier   
+# from sklearn.neighbors import KNeighborsClassifier
 
 
-st.set_page_config(page_title="Credit Card Fraud Message Detector", layout="centered")
+st.set_page_config(
+    page_title="Credit Card Fraud Message Detector", layout="centered")
 
 st.title("💳 Credit Card Fraud Message Detector")
 
@@ -18,12 +19,13 @@ df = pd.read_csv("data.csv")
 model = LogisticRegression(max_iter=1000, class_weight="balanced")
 
 
-#model = KNeighborsClassifier(n_neighbors=5)
-df['label_num'] = df['label'].map({'fraud': 0, 'safe': 1})        
+# model = KNeighborsClassifier(n_neighbors=5)
+df['label_num'] = df['label'].map({'fraud': 0, 'safe': 1})
 x = df['message']
 y = df['label_num']
 
-X_train, X_test, y_train, y_act = train_test_split(x, y, test_size=0.2, random_state=42)
+X_train, X_test, y_train, y_act = train_test_split(
+    x, y, test_size=0.2, random_state=42)
 
 vectorizer = TfidfVectorizer()
 X_train_vec = vectorizer.fit_transform(X_train)
@@ -31,11 +33,10 @@ X_test_vec = vectorizer.transform(X_test)
 joblib.dump((vectorizer, model), "fraud_model.pkl")
 
 
-
 model.fit(X_train_vec, y_train)
 
 
-#_____main_____
+# _____main_____
 
 user_input = st.text_input(label="Enter the message to check for fraud:")
 
@@ -45,7 +46,7 @@ if st.button("Check Message"):
     else:
         user_input_vec = vectorizer.transform([user_input])
         pre = model.predict(user_input_vec)[0]
-        st.write(pre)   
+        st.write(pre)
         prediction = model.predict(user_input_vec)[0]
         prediction.round(2)
         proba = model.predict_proba(user_input_vec)[0]
@@ -56,11 +57,8 @@ if st.button("Check Message"):
         st.write(f"Safe Probability: {safe_score:.2f}")
         vectorizer, model = joblib.load("fraud_model.pkl")
 
-
         if prediction == 1:
             st.error("✅ This is a SAFE message.")
-            
+
         else:
             st.success("⚠️ This is a FRAUD message.")
-
-
